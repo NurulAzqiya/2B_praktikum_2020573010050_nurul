abef9bc847934ff2ffbb3955dc4156e5976d4d45
NurulAzqiya <nurulazkiya290@gmail.com>
2021-10-07 15:53:28 +0700

    menambah file latihan 3.php

---
+++ praktikum_5_2020573010050_Nurul/proses_login.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Proses Input</title>
+</head>
+<body>
+    <?php 
+        $username=$_POST["username"];
+        $password=$_POST["password"];
+    ?>
+    Username : <?echo $username?>
+    <br>
+    Password : <?echo $password?>
+    <br>
+</body>
+</html> 
