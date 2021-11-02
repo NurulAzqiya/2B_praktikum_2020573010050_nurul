0fb0008753800072e8cc02fb79da28707cc55504
NurulAzqiya <nurulazkiya290@gmail.com>
2021-11-02 17:05:32 +0700

    menambahkan file praktikum 6

---
+++ praktikum_7_2020573010050_Nurul/sidebar.php
@@ -0,0 +1,33 @@
+<div class="d-flex flex-column flex-shrink-0 p-3 bg-light" style="width: 280px">
+    <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
+    <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap" /></svg>
+    <span class="fs-4">Sidebar</span>
+    </a>
+    <hr />
+    <ul class="nav nav-pills flex-column mb-auto">
+        <li class="nav-item">
+            <a href="index.php" class="nav-link active" aria-current="page">
+            <i class="bi me-2 bi-house-door-fill"></i>
+                Home
+            </a>
+        </li>
+        <li>
+            <a href="data_barang.php" class="nav-link link-dark">
+            <i class="bi me-2 bi-briefcase"></i>
+                Data Barang
+            </a>
+        </li>
+        <li>
+            <a href="data_mahasiswa.php" class="nav-link link-dark">
+            <i class="bi me-2 bi-people"></i>
+                Data Mahasiswa
+            </a>
+        </li>
+        <li>
+            <a href="#" class="nav-link link-dark">
+            <i class="bi me-2 bi-grid"></i>
+                Data Peminjaman
+            </a>
+        </li>
+    </ul>
+</div>
