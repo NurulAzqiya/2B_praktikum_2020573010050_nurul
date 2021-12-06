a61eebb7a0b671fe9ba072413236b55de126c946
NurulAzqiya <nurulazkiya290@gmail.com>
2021-12-06 09:14:26 +0700

    menambahkan file praktikum 10

---
+++ praktikum_8_2020573010050_Nurul/header.php
@@ -0,0 +1,22 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+        <a class="navbar-brand fw-bold ms-4" href="#">SIPBAR</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="dropdown me-5">
+            <a href="#" class="d-flex align-items-center link dark text-decoration-none dropdown-toggle" id="dropdownUser2" data-bs-toggle="dropdown" aria-expanded="false">
+                <img src="image.JPG" alt="" width="32" height="32" class="rounded-circle me-2" />
+                <strong>Nurul Azqiya</strong>
+            </a>
+            <ul class="dropdown-menu text-small shadow" aria-labelledby="dropdownUser2">
+                <li><a class="dropdown-item" href="#">Settings</a></li>
+                <li><a class="dropdown-item" href="#">Profile</a></li>
+                <li>
+                    <hr class="dropdown-divider" />
+                </li>
+                <li><a class="dropdown-item" href="login.php">Sign out</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
---
+++ praktikum_8_2020573010050_Nurul/sidebar.php
@@ -0,0 +1,29 @@
+<div class"d-flex flex-column flex-shrink-0 p-3 bg-light" style="width: 280px;">
+    <hr>
+        <ul class="nav nav-pills flex-column mb-auto">
+            <li class="nav-item">
+                <a href="index.php" class="nav-link active" aria-current="page">
+                    <svg class="bi me-2" widht="16" height="16"><use xlink:href="#home"/></svg>
+                    Home
+                </a>
+            </li>
+
+            <li>
+                <a href="data_barang.php" class="nav-link link-dark">
+                    <svg class=bi me" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-grid-fill" viewBox="0 0 16 16">
+                        <path d="M1 2.5A1.5 1.5 0 0 1 2.5 1h3A1.5 1.5 0 0 1 7 2.5v3A1.5 1.5 0 0 1 5.5 7h-3A1.5 1.5 0 0 1 1 5.5v-3zm8 0A1.5 1.5 0 0 1 10.5 1h3A1.5 1.5 0 0 1 15 2.5v3A1.5 1.5 0 0 :"
+                    </svg>
+                    Data Barang
+                </a>
+            </li>
+
+            <li>
+                <a href="data_dosen.php" class="nav-link link-dark">
+                    <svg class="bi me-2" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-people-fill" viewBox="0 0 16 16">
+                        <path d="M7 14s-1 0-1-1 1-4 5-4 5 3 5 4-1 1-1 1H7zm4-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
+                        <path fill-rule="evenodd d="M5.216 14A2.238 2.238 0 0 1 5 13c0-1.355.68-2.75 1.936-3.72A6.325 0 0 0 5
+                    </svg>
+                </a>
+            </li>
+        </ul>
+</div>
